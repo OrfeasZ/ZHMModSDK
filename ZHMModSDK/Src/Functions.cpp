dd5a6afc07a706cb255bdeacb9358a2a32471b13
OrfeasZ <orfeaz@gmail.com>
2021-06-16 18:22:01 +0300

    Update for Hitman 3.40.0.0

--- ZHMModSDK/Src/Functions.cpp
+++ ZHMModSDK/Src/Functions.cpp
@@ -72,8 +72,8 @@ PATTERN_FUNCTION(
 );
 
 PATTERN_RELATIVE_FUNCTION(
-	"\xE8\x00\x00\x00\x00\x48\x8B\x44\x24\x48\x48\x8B\xB0\x40\x11\x00\x00",
-	"x????xxxxxxxxxxxx",
+	"\xE8\x00\x00\x00\x00\x48\x8B\x4C\x24\x38\x48\x85\xC9\x74\x00\x48\x81\xC1",
+	"x????xxxxxxxxx?xxx",
 	ZPlayerRegistry_GetLocalPlayer,
 	void(ZPlayerRegistry* th, TEntityRef<ZHitman5>* out)
 );
