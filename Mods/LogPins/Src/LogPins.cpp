a0a1c705fa5d26d082b87c2e7c3b279086bdea5a
Thomas <piepieonline@gmail.com>
2022-01-11 21:16:17 +1100

    Add files via upload

---
+++ Mods/LogPins/Src/LogPins.cpp
@@ -0,0 +1,31 @@
+#include "AdvancedRating.h"
+
+#include "Hooks.h"
+#include "Logging.h"
+#include "Functions.h"
+
+#include <Glacier/ZScene.h>
+#include <Glacier/ZAIGameState.h>
+#include <Glacier/SOnlineEvent.h>
+
+#include "json.hpp"
+
+void LogPins::PreInit()
+{
+	Hooks::SignalInputPin->AddDetour(this, &LogPins::SignalInputPin);
+	Hooks::SignalOutputPin->AddDetour(this, &LogPins::SignalOutputPin);
+	Hooks::ZAchievementManagerSimple_OnEventSent->AddDetour(this, &AdvancedRating::ZAchievementManagerSimple_OnEventSent);
+}
+
+
+DEFINE_PLUGIN_DETOUR(LogPins, bool, SignalInputPin, ZEntityRef zEntityRef, uint32_t pinId, const ZObjectRef& zObjectRef)
+{
+	Logger::Debug("Pin Input: {}", pinId);
+}
+
+DEFINE_PLUGIN_DETOUR(LogPins, bool, SignalOutputPin, ZEntityRef zEntityRef, uint32_t pinId, const ZObjectRef& zObjectRef);
+{
+	Logger::Debug("Pin Output: {}", pinId);
+}
+
+DECLARE_ZHM_PLUGIN(LogPins);
