9950e0db3ad54c3314cc774bec6f46165afc8995
Thomas <piepieonline@gmail.com>
2022-01-12 06:59:45 +1100

    Update LogPins.h

--- Mods/LogPins/Src/LogPins.h
+++ Mods/LogPins/Src/LogPins.h
@@ -13,6 +13,10 @@ public:
 private:
 	DEFINE_PLUGIN_DETOUR(LogPins, bool, SignalInputPin, ZEntityRef, uint32_t, const ZObjectRef&);
 	DEFINE_PLUGIN_DETOUR(LogPins, bool, SignalOutputPin, ZEntityRef, uint32_t, const ZObjectRef&);
+	
+private:
+	std::unordered_map<uint32_t, bool> m_knownInputs;
+	std::unordered_map<uint32_t, bool> m_knownOutputs;
 };
 
 DEFINE_ZHM_PLUGIN(LogPins)
