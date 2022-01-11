33b20f0a6b7ef6e36fc2d801f93b295fd588617e
Thomas <piepieonline@gmail.com>
2022-01-12 07:12:14 +1100

    Update LogPins.cpp

--- Mods/LogPins/Src/LogPins.cpp
+++ Mods/LogPins/Src/LogPins.cpp
@@ -14,14 +14,26 @@ void LogPins::PreInit()
 
 DECLARE_PLUGIN_DETOUR(LogPins, bool, SignalInputPin, ZEntityRef zEntityRef, uint32_t pinId, const ZObjectRef& zObjectRef)
 {
-	Logger::Info("Pin Input: {}", pinId);
+	auto it = m_knownInputs.find(pinId);
+	if (it == m_knownInputs.end())
+	{
+		Logger::Info("Pin Input: {}", pinId);
+		
+		m_knownInputs[pinId] = true;
+	}
 
 	return HookResult<bool>(HookAction::Continue());
 }
 
 DECLARE_PLUGIN_DETOUR(LogPins, bool, SignalOutputPin, ZEntityRef entityRef, uint32_t pinId, const ZObjectRef& objectRef)
 {
-	Logger::Info("Pin Output: {}", pinId);
+	auto it = m_knownOutputs.find(pinId);
+	if (it == m_knownOutputs.end())
+	{
+		Logger::Info("Pin Output: {}", pinId);
+		
+		m_knownOutputs[pinId] = true;
+	}
 
 	return HookResult<bool>(HookAction::Continue());
 }
