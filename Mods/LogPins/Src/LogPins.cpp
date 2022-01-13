c80443b13d84d237fc241949beca0d7edcaa2321
Thomas <piepieonline@gmail.com>
2022-01-13 18:47:52 +1100

    Update LogPins.cpp

--- Mods/LogPins/Src/LogPins.cpp
+++ Mods/LogPins/Src/LogPins.cpp
@@ -8,17 +8,6 @@
 #include <Glacier/ZEntity.h>
 #include <Glacier/ZObject.h>
 
-#ifndef _WINDOWS_
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#undef WIN32_LEAN_AND_MEAN
-#endif
-
-#include <winsock2.h>
-
-#pragma comment(lib, "ws2_32.lib")
-
-
 #define DEFAULT_SERVER "127.0.0.1"
 #define DEFAULT_BUFLEN 512
 #define DEFAULT_PORT 27015
