a926d5dd1c3c84ab9eb622e799013ef2d5b85d74
OrfeasZ <orfeaz@gmail.com>
2023-01-26 18:22:41 +0200

    Update for Hitman 3.140.0.0

--- ZHMModSDK/Include/Glacier/EntityFactory.h
+++ ZHMModSDK/Include/Glacier/EntityFactory.h
@@ -4,10 +4,72 @@
 
 class IEntityFactory : public IComponentInterface
 {
+public:
+	virtual void IEntityFactory_unk5() = 0;
+	virtual void ConfigureEntity() = 0;
+	virtual void IEntityFactory_unk7() = 0;
+	virtual void IEntityFactory_unk8() = 0;
+	virtual void IEntityFactory_unk9() = 0;
+	virtual void IEntityFactory_unk10() = 0;
+};
+
+class IEntityBlueprintFactory : public IComponentInterface
+{
+public:
+	virtual void IEntityBlueprintFactory_unk5() = 0;
+	virtual void IEntityBlueprintFactory_unk6() = 0;
+	virtual void IEntityBlueprintFactory_unk7() = 0;
+	virtual void IEntityBlueprintFactory_unk8() = 0;
+	virtual void IEntityBlueprintFactory_unk9() = 0;
+	virtual void IEntityBlueprintFactory_unk10() = 0;
+	virtual void IEntityBlueprintFactory_unk11() = 0;
+	virtual void IEntityBlueprintFactory_unk12() = 0;
+	virtual void IEntityBlueprintFactory_unk13() = 0;
+	virtual void IEntityBlueprintFactory_unk14() = 0;
+	virtual void IEntityBlueprintFactory_unk15() = 0;
+	virtual void IEntityBlueprintFactory_unk16() = 0;
+	virtual void IEntityBlueprintFactory_unk17() = 0;
+	virtual void IEntityBlueprintFactory_unk18() = 0;
+	virtual void IEntityBlueprintFactory_unk19() = 0;
+	virtual bool IEntityBlueprintFactory_unk20() = 0;
+	virtual int64_t GetSubEntitiesCount() = 0;
+	virtual void IEntityBlueprintFactory_unk22() = 0;
+	virtual void IEntityBlueprintFactory_unk23() = 0;
+	virtual void IEntityBlueprintFactory_unk24() = 0;
+	virtual void IEntityBlueprintFactory_unk25() = 0;
+	virtual void IEntityBlueprintFactory_unk26() = 0;
+	virtual void IEntityBlueprintFactory_unk27() = 0;
+	virtual void IEntityBlueprintFactory_unk28() = 0;
+	virtual void IEntityBlueprintFactory_unk29() = 0;
+	virtual void IEntityBlueprintFactory_unk30() = 0;
+	virtual void IEntityBlueprintFactory_unk31() = 0;
+	virtual void IEntityBlueprintFactory_unk32() = 0;
+};
+
+class ZEntityBlueprintFactoryBase : public IEntityBlueprintFactory
+{
+public:
+	PAD(0x40);
+};
+
+class ZCompositeEntityBlueprintFactoryBase : public ZEntityBlueprintFactoryBase
+{
+public:
+	TArray<int64_t> m_aSubEntityOffsets; // 0x48
+};
+
+static_assert(offsetof(ZCompositeEntityBlueprintFactoryBase, m_aSubEntityOffsets) == 0x48);
+
+class ZTemplateEntityBlueprintFactory : public ZCompositeEntityBlueprintFactoryBase
+{
 	
 };
 
 class ZTemplateEntityFactory : public IEntityFactory
 {
-	
+public:
+	PAD(0x30);
+	TResourcePtr<ZTemplateEntityBlueprintFactory> m_blueprintResource; // 0x38
 };
+
+static_assert(offsetof(ZTemplateEntityFactory, m_blueprintResource) == 0x38);
--- ZHMModSDK/Include/Glacier/ZRender.h
+++ ZHMModSDK/Include/Glacier/ZRender.h
@@ -56,12 +56,15 @@ public:
 	virtual ~ZRenderDevice() = default;
 
 public:
-	PAD(0x408);
-	ZRenderSwapChain* m_pSwapChain; // 0x410, look for ZRenderSwapChain constructor
-	ID3D12Device* m_pDevice; // 0x418
-	PAD(0x30FA350); // 0x420
-	ID3D12CommandQueue* m_pCommandQueue; // 0x30FA770, look for "m_pFrameHeapCBVSRVUAV" string, first vtable call with + 128
+	PAD(0x10A08);
+	ZRenderSwapChain* m_pSwapChain; // 0x10A10, look for ZRenderSwapChain constructor
+	ID3D12Device* m_pDevice; // 0x10A18
+	PAD(0x30E9D90); // 0x10A20
+	ID3D12CommandQueue* m_pCommandQueue; // 0x30FA7B0, look for "m_pFrameHeapCBVSRVUAV" string, first vtable call with + 128
 };
+
+static_assert(offsetof(ZRenderDevice, m_pSwapChain) == 0x10A10);
+static_assert(offsetof(ZRenderDevice, m_pCommandQueue) == 0x30FA7B0);
 
 class ZRenderContext
 {
@@ -78,8 +81,8 @@ public:
 	virtual ~ZRenderManager() = default;
 
 public:
-	PAD(0x14170);
-	ZRenderDevice* m_pDevice; // 0x14178, look for ZRenderDevice constructor
-	PAD(0x100); // 0x14180
+	PAD(0x14178);
+	ZRenderDevice* m_pDevice; // 0x14180, look for ZRenderDevice constructor
+	PAD(0xF8); // 0x14188
 	ZRenderContext* m_pRenderContext; // 0x14280, look for "ZRenderManager::RenderThread" string, first thing being constructed and assigned
 };
