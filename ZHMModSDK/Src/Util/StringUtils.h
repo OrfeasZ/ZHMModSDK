baf8074b3a26139cd65d00ebfd57e1e893ef7623
Deji <the_zone@hotmail.co.uk>
2023-08-22 00:38:28 +0100

    Possibly the last mixed indent fix

--- ZHMModSDK/Src/Util/StringUtils.h
+++ ZHMModSDK/Src/Util/StringUtils.h
@@ -11,6 +11,6 @@ namespace Util
         static std::vector<std::string> Split(std::string_view p_String, const std::string& p_Delimeter);
         static std::string ToLowerCase(const std::string& p_String);
         static std::string ToUpperCase(const std::string& p_String);
-		static bool CompareInsensitive(std::string_view p_StringA, std::string_view p_StringB);
+        static bool CompareInsensitive(std::string_view p_StringA, std::string_view p_StringB);
     };
 }
