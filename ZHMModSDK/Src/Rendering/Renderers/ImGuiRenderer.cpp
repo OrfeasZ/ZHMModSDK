339f3a253b0266c01609108e01dbe56f720e5c07
Pavle <pavle_nis@yahoo.com>
2025-09-13 07:44:02 +0200

    Prevent buffered events from being processed by the game after imgui is closed

--- ZHMModSDK/Src/Rendering/Renderers/ImGuiRenderer.cpp
+++ ZHMModSDK/Src/Rendering/Renderers/ImGuiRenderer.cpp
@@ -626,6 +626,17 @@ DEFINE_DETOUR_WITH_CONTEXT(
             // Set the GUI to visible again if we toggle it on.
             m_ImguiVisible = true;
         }
+
+        if (!m_ImguiHasFocus) {
+            DWORD s_EventCount = 256;
+            DIDEVICEOBJECTDATA s_Buffer[256];
+            ZKeyboardWindows* s_KeyboardWindows = static_cast<ZKeyboardWindows*>(Globals::InputDeviceManager->m_devices[3]);
+
+            if (s_KeyboardWindows->dif.m_pDev) {
+                // Prevents buffered events from being processed by the game after imgui is closed
+                s_KeyboardWindows->dif.m_pDev->GetDeviceData(sizeof(DIDEVICEOBJECTDATA), s_Buffer, &s_EventCount, 0);
+            }
+        }
     }
 
     if (s_ScanCode == ModSDK::GetInstance()->GetUiToggleScanCode() && (p_Message == WM_KEYDOWN || p_Message ==
