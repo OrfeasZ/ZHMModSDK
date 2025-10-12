d17de63132cca66383107a1a74335b16020ca623
Pavle <pavle_nis@yahoo.com>
2025-10-12 16:42:39 +0200

    Add hook for ZUIText::TryGetTextFromNameHash method

--- ZHMModSDK/Include/Hooks.h
+++ ZHMModSDK/Include/Hooks.h
@@ -200,4 +200,6 @@ public:
     )>* ZPathfinder_CreateObstacle;
     static Hook<void(ZPFObstacleEntity* th, uint32 nObstacleBlockageFlags, bool bEnabled, bool forceUpdate)>*
     ZPFObstacleEntity_UpdateObstacle;
+
+    static Hook<bool(ZUIText* th, int32 nNameHash, ZString& sResult, int& outMarkupResult)>* ZUIText_TryGetTextFromNameHash;
 };
