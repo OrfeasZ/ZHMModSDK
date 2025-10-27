f75c896bcee76d3d128859d3a189b66b55996b29
Pavle <pavle_nis@yahoo.com>
2025-10-27 07:21:26 +0100

    Rename methods in IComponentInterface

--- ZHMModSDK/Include/Glacier/Reflection.h
+++ ZHMModSDK/Include/Glacier/Reflection.h
@@ -11,10 +11,10 @@ class ZObjectRef;
 class IComponentInterface {
 public:
     virtual ~IComponentInterface() = 0;
-    virtual ZObjectRef* getAsObjectRef(ZObjectRef* result) = 0;
-    virtual int addRef() = 0;
-    virtual int release() = 0;
-    virtual void* getSubclassStart(STypeID* type) = 0;
+    virtual ZObjectRef* GetVariantRef(ZObjectRef& result) = 0;
+    virtual int AddRef() = 0;
+    virtual int Release() = 0;
+    virtual void* QueryInterface(STypeID* iid) = 0;
 };
 
 enum ETypeInfoFlags : uint16_t {
