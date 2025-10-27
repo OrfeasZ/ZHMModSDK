7701b11064f7544f98cdaaa440dd3c31d01baeed
Pavle <pavle_nis@yahoo.com>
2025-10-27 07:22:32 +0100

    Rename m_DynamicEntities to m_Entities in ZEntityManager

--- ZHMModSDK/Include/Glacier/ZEntityManager.h
+++ ZHMModSDK/Include/Glacier/ZEntityManager.h
@@ -13,7 +13,7 @@ public:
 public:
     ZEntitySceneContext* m_pContext; // 0x8
     PAD(0x18); // 0x10
-    THashMap<uint64_t, ZEntityRef> m_DynamicEntities; // 0x28
+    THashMap<uint64_t, ZEntityRef> m_Entities; // 0x28
     PAD(0xC0); // 0x48
     THashMap<uint64_t, uint64_t> m_DynamicEntityIdToCount; // 0x108
 };
