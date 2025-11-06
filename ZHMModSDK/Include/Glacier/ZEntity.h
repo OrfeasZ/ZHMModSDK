7dc7e3c8208619d6637ab4aee9134515499f1996
Pavle <pavle_nis@yahoo.com>
2025-11-06 19:33:25 +0100

    Remove unnecessary inline specifier from IsTemplateEntityBlueprintFactory and IsAspectEntityBlueprintFactory

--- ZHMModSDK/Include/Glacier/ZEntity.h
+++ ZHMModSDK/Include/Glacier/ZEntity.h
@@ -54,11 +54,9 @@ public:
     virtual void IEntityBlueprintFactory_unk31() = 0;
     virtual void IEntityBlueprintFactory_unk32() = 0;
 
-    inline bool IsTemplateEntityBlueprintFactory() const {
         return Globals::ZTemplateEntityBlueprintFactory_vtbl == *(void**) this;
     }
 
-    inline bool IsAspectEntityBlueprintFactory() const {
         return Globals::ZAspectEntityBlueprintFactory_vtbl == *(void**)this;
     }
 };
