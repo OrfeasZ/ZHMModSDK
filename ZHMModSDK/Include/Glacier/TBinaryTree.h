071b3e3ae53d4d9423a086a4a6496a0cd470b2d8
Pavle <pavle_nis@yahoo.com>
2025-11-10 13:29:52 +0100

    Refactor TBinaryTree and TMap to improve traversal, iterators, and add contains method

--- ZHMModSDK/Include/Glacier/TBinaryTree.h
+++ ZHMModSDK/Include/Glacier/TBinaryTree.h
@@ -1,124 +1,165 @@
 #pragma once
 
+#include "TIterator.h"
+
 template <typename T>
 class TBinaryTreeNode {
 public:
-    static TBinaryTreeNode* GetNextNode(TBinaryTreeNode* pNode) {
-        TBinaryTreeNode* result = pNode->m_pRight;
+    static TBinaryTreeNode* GetNextNode(TBinaryTreeNode* p_Node) {
+        // Case 1: node has right child -> go to leftmost node in right subtree
+        TBinaryTreeNode* s_CurrentNode = p_Node;
+        if (s_CurrentNode->m_pRight) {
+            TBinaryTreeNode* s_SuccessorNode = s_CurrentNode->m_pRight;
 
-        if (result) {
-            for (TBinaryTreeNode* i = result->m_pLeft; i; i = i->m_pLeft) {
-                result = i;
-            }
-        }
-        else {
-            result = pNode->m_pParent;
-
-            if (result) {
-                if (result->m_pLeft == pNode) {
-                    return result;
-                }
-
-                do {
-                    if (result->m_pLeft == pNode) {
-                        break;
-                    }
-
-                    pNode = result;
-                    result = result->m_pParent;
-                } while (result);
+            while (s_SuccessorNode->m_pLeft) {
+                s_SuccessorNode = s_SuccessorNode->m_pLeft;
             }
 
-            result = pNode->m_pParent;
-
-            if (!result) {
-                return pNode;
-            }
+            return s_SuccessorNode;
         }
 
-        return result;
+        // Case 2: no right child -> go up until the current node is a left child
+        TBinaryTreeNode* s_ParentNode = s_CurrentNode->m_pParent;
+
+        if (!s_ParentNode) {
+            return s_CurrentNode;
+        }
+
+        while (s_ParentNode && s_CurrentNode != s_ParentNode->m_pLeft) {
+            s_CurrentNode = s_ParentNode;
+            s_ParentNode = s_ParentNode->m_pParent;
+        }
+
+        return s_ParentNode;
     }
 
 public:
-    int m_nBalance; // 0x00
-    TBinaryTreeNode<T>* m_pParent; // 0x08
-    TBinaryTreeNode<T>* m_pLeft; // 0x10
-    TBinaryTreeNode<T>* m_pRight; // 0x18
-    T m_data; // 0x20
+    int m_nBalance;
+    TBinaryTreeNode<T>* m_pParent;
+    TBinaryTreeNode<T>* m_pLeft;
+    TBinaryTreeNode<T>* m_pRight;
+    T m_data;
 };
 
 template <typename T>
-class TBinaryTreeIterator {
+class TBinaryTreeIterator : public TIterator<T> {
 public:
-    TBinaryTreeIterator() :
-        m_pCurrent(nullptr) {
+    TBinaryTreeIterator()
+        : TIterator<T>(nullptr)
+    {
     }
 
-    TBinaryTreeIterator(T* ptr) :
-        m_pCurrent(ptr) {
+    explicit TBinaryTreeIterator(T* ptr)
+        : TIterator<T>(ptr)
+    {
     }
 
     TBinaryTreeIterator<T>& operator++() {
-        TBinaryTreeNode<T>* node = reinterpret_cast<TBinaryTreeNode<T>*>(reinterpret_cast<char*>(this->m_pCurrent) -
-            offsetof(TBinaryTreeNode<T>, TBinaryTreeNode<T>::m_data));
+        using NodeType = TBinaryTreeNode<std::remove_const_t<T>>;
 
-        this->m_pCurrent = &TBinaryTreeNode<T>::GetNextNode(node)->m_data;
+        const NodeType* s_Node = reinterpret_cast<const NodeType*>(
+            reinterpret_cast<const char*>(this->m_pCurrent) - offsetof(NodeType, m_data)
+        );
+
+        NodeType* s_Next = NodeType::GetNextNode(const_cast<NodeType*>(s_Node));
+        this->m_pCurrent = &s_Next->m_data;
 
         return *this;
     }
 
     bool operator==(const TBinaryTreeIterator<T>& other) const {
-        return m_pCurrent == other.m_pCurrent;
+        return this->m_pCurrent == other.m_pCurrent;
     }
 
     bool operator!=(const TBinaryTreeIterator<T>& other) const {
-        return m_pCurrent != other.m_pCurrent;
+        return this->m_pCurrent != other.m_pCurrent;
     }
 
     T& operator*() const {
-        return *m_pCurrent;
+        return *this->m_pCurrent;
     }
 
     T* operator->() const {
-        return m_pCurrent;
+        return this->m_pCurrent;
     }
-
-public:
-    T* m_pCurrent;
 };
 
 template <typename T>
 class TBinaryTree {
 public:
     struct SFakeTreeNode {
-        int m_nReserved; // 0x00
-        TBinaryTreeNode<T>* m_pNULL; // 0x08
-        TBinaryTreeNode<T>* m_pRightRoot; // 0x10
-        TBinaryTreeNode<T>* m_pLeftRoot; // 0x18
+        int m_nReserved;
+        TBinaryTreeNode<T>* m_pNULL;
+        TBinaryTreeNode<T>* m_pRightRoot;
+        TBinaryTreeNode<T>* m_pLeftRoot;
     };
 
     using iterator = TBinaryTreeIterator<T>;
-    using const_iterator = const TBinaryTreeIterator<T>;
+    using const_iterator = TBinaryTreeIterator<const T>;
 
 public:
-    iterator end() {
-        return reinterpret_cast<T*>(&m_nSize);
+    const_iterator begin() const {
+        if (m_tree.m_pLeftRoot) {
+            using NodeType = TBinaryTreeNode<std::remove_const_t<T>>;
+
+            NodeType* s_FakeRootNode = reinterpret_cast<NodeType*>(const_cast<TBinaryTree<T>*>(this));
+            NodeType* s_FirstNode = NodeType::GetNextNode(s_FakeRootNode);
+
+            return const_iterator(&s_FirstNode->m_data);
+        }
+
+        return const_iterator(reinterpret_cast<const T*>(&m_nSize));
     }
 
     iterator begin() {
-        iterator result;
-
         if (m_tree.m_pLeftRoot) {
-            result.m_pCurrent = &TBinaryTreeNode<T>::GetNextNode(reinterpret_cast<TBinaryTreeNode<T>*>(this))->m_data;
-        }
-        else {
-            result.m_pCurrent = reinterpret_cast<T*>(&m_nSize);
+            using NodeType = TBinaryTreeNode<std::remove_const_t<T>>;
+
+            NodeType* s_FakeRootNode = reinterpret_cast<NodeType*>(this);
+            NodeType* s_FirstNode = NodeType::GetNextNode(s_FakeRootNode);
+
+            return iterator(&s_FirstNode->m_data);
         }
 
-        return result;
+        return iterator(reinterpret_cast<T*>(&m_nSize));
+    }
+
+    const_iterator end() const {
+        return const_iterator(reinterpret_cast<const T*>(&m_nSize));
+    }
+
+    iterator end() {
+        return iterator(reinterpret_cast<T*>(&m_nSize));
+    }
+
+    size_t size() const {
+        return m_nSize;
+    }
+
+    bool contains(const T& p_Value) const {
+        return find(p_Value) != end();
+    }
+
+    iterator find(const T& p_Value) {
+        auto* s_Node = find(m_tree.m_pLeftRoot, p_Value);
+
+        return s_Node ? iterator(&s_Node->m_data) : end();
+    }
+
+private:
+    TBinaryTreeNode<T>* find(TBinaryTreeNode<T>* p_Root, const T& p_Value) {
+        if (!p_Root || p_Root->m_data.first == p_Value) {
+            return p_Root;
+        }
+
+        if (p_Root->m_data < p_Value) {
+            return find(p_Root->m_pRight, p_Value);
+        }
+
+        return find(p_Root->m_pLeft, p_Value);
     }
 
 public:
-    SFakeTreeNode m_tree; // 0x00
-    int m_nSize; // 0x20
+    SFakeTreeNode m_tree;
+    int m_nSize;
 };
--- ZHMModSDK/Include/Glacier/TMap.h
+++ ZHMModSDK/Include/Glacier/TMap.h
@@ -3,49 +3,63 @@
 #include "TPair.h"
 #include "TRedBlackTree.h"
 
-template <typename T, typename Z>
+template <typename TKeyType, typename TValueType>
 class TMap {
 public:
-    using value_type = TPair<T, Z>;
+    using value_type = TPair<TKeyType, TValueType>;
 
     using iterator = TBinaryTreeIterator<value_type>;
-    using const_iterator = const TBinaryTreeIterator<value_type>;
+    using const_iterator = TBinaryTreeIterator<const value_type>;
 
 public:
-    inline iterator end() {
-        return m_container.end();
-    }
-
-    inline iterator begin() {
+    const_iterator begin() const {
         return m_container.begin();
     }
 
-    inline iterator find(const T& key) {
-        TBinaryTreeNode<value_type>* node = find(m_container.m_tree.m_pLeftRoot, key);
-
-        if (node) {
-            return &node->m_data;
-        }
-
-        return end();
+    iterator begin() {
+        return m_container.begin();
     }
 
-    inline TBinaryTreeNode<value_type>* find(TBinaryTreeNode<value_type>* root, const T& key) {
-        if (!root || root->m_data.first == key) {
-            return root;
-        }
-
-        if (root->m_data.first < key) {
-            return find(root->m_pRight, key);
-        }
-
-        return find(root->m_pLeft, key);
+    const_iterator end() const {
+        return m_container.end();
     }
 
-    inline size_t size() const {
+    iterator end() {
+        return m_container.end();
+    }
+
+    bool contains(const TKeyType& p_Key) const {
+        return find(p_Key) != end();
+    }
+
+    const_iterator find(const TKeyType& p_Key) const {
+        auto* s_Node = find(m_container.m_tree.m_pLeftRoot, p_Key);
+
+        return s_Node ? const_iterator(&s_Node->m_data) : end();
+    }
+
+    iterator find(const TKeyType& p_Key) {
+        auto* s_Node = find(m_container.m_tree.m_pLeftRoot, p_Key);
+
+        return s_Node ? iterator(&s_Node->m_data) : end();
+    }
+
+    size_t size() const {
         return m_container.m_nSize;
     }
 
 protected:
+    TBinaryTreeNode<value_type>* find(TBinaryTreeNode<value_type>* p_Root, const TKeyType& p_Key) {
+        if (!p_Root || p_Root->m_data.first == p_Key) {
+            return p_Root;
+        }
+
+        if (p_Root->m_data.first < p_Key) {
+            return find(p_Root->m_pRight, p_Key);
+        }
+
+        return find(p_Root->m_pLeft, p_Key);
+    }
+
     TRedBlackTree<value_type> m_container;
 };
