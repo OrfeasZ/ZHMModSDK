48067f297b3b29c217a062cc223acd243d983b71
Pavle <pavle_nis@yahoo.com>
2025-12-27 12:47:50 +0100

    Load scene names, code name hints and titles from JSON files instead of hardcoding them in DiscordRichPresence mod

--- Mods/DiscordRichPresence/Src/DiscordRichPresence.h
+++ Mods/DiscordRichPresence/Src/DiscordRichPresence.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <random>
 #include <unordered_map>
 
 #include "IPluginInterface.h"
@@ -18,19 +17,16 @@ public:
     void OnEngineInitialized() override;
 
 private:
-    void PopulateScenes();
-    void PopulateGameModes();
-    void PopulateCodenameHints();
-    std::string LowercaseString(const std::string& p_In) const;
-    std::string FindLocationForScene(ZString p_Scene) const;
+    void BuildSceneMappings();
+    void BuildGameModeMappings();
 
 private:
-    DECLARE_PLUGIN_DETOUR(DiscordRichPresence, bool, OnLoadScene, ZEntitySceneContext*, SSceneInitParameters&);
+    DECLARE_PLUGIN_DETOUR(DiscordRichPresence, void, OnCreateScene, ZEntitySceneContext* th, bool p_ResetScene);
 
 private:
-    std::unordered_map<std::string_view, const char*> m_Scenes;
-    std::unordered_map<std::string_view, const char*> m_GameModes;
-    std::unordered_map<std::string_view, const char*> m_CodenameHints;
+    std::unordered_map<std::string, std::string> m_CodeNameHintToSceneName;
+    std::unordered_map<std::string, std::string> m_CodeNameHintToTitle;
+    std::unordered_map<std::string, std::string> m_TypeToGameMode;
     discord::Core* m_DiscordCore;
 };
 
