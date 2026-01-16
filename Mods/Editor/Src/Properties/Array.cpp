2a7486ff63b1359577e4195e8f5153709c2859df
Pavle <pavle_nis@yahoo.com>
2026-01-16 09:42:27 +0100

    Move array element rendering logic to DrawArrayElements

--- Mods/Editor/Src/Properties/Array.cpp
+++ Mods/Editor/Src/Properties/Array.cpp
@@ -33,85 +33,55 @@ void Editor::TArrayProperty(const std::string& p_Id, ZEntityRef p_Entity, ZEntit
     ImGui::PopFont();
 
     if (s_IsTreeNodeOpen) {
-        const STypeID* s_ElementTypeID = s_ArrayType->m_pArrayElementType;
-        const std::string s_ElementTypeName = s_ElementTypeID->typeInfo()->m_pTypeName;
-
-        void* s_Iterator = s_ArrayType->m_pArrayFunctions->begin(p_Data);
-        void* s_End = s_ArrayType->m_pArrayFunctions->end(p_Data);
-
-        int index = 0;
-        for (; s_Iterator != s_End; s_Iterator = s_ArrayType->m_pArrayFunctions->next(p_Data, s_Iterator), ++index) {
-            ImGui::PushID(static_cast<int>(index));
-
-            ImGui::Separator();
-
-            ImGui::AlignTextToFramePadding();
-            ImGui::Text("[%zu]", index);
-
-            ImGui::SameLine();
-
-            const std::string s_ElementId = std::format("{}[{}]", p_Id, index);
-
-            DrawEntityPropertyValue(
-                s_ElementId,
-                "",
-                s_ElementTypeName,
-                s_ElementTypeID,
-                p_Entity,
-                p_Property,
-                s_Iterator
-            );
-
-            ImGui::PopID();
-        }
+        DrawArrayElements(
+            p_Id,
+            p_Entity,
+            p_Property,
+            p_Data,
+            s_ArrayType
+        );
 
         ImGui::TreePop();
     }
 }
-    }
 
-    ImGui::PushFont(SDK()->GetImGuiBoldFont());
-    ImGui::AlignTextToFramePadding();
+void Editor::DrawArrayElements(
+    const std::string& p_Id,
+    ZEntityRef p_Entity,
+    ZEntityProperty* p_Property,
+    void* p_Data,
+    const IArrayType* s_ArrayType
+) {
+    const STypeID* s_ElementTypeID = s_ArrayType->m_pArrayElementType;
+    const std::string s_ElementTypeName = s_ElementTypeID->typeInfo()->m_pTypeName;
 
-    const bool s_IsTreeNodeOpen = ImGui::TreeNodeEx(
-        fmt::format("{} ({} {})##{}",
-            s_PropertyName,
-            s_ArraySize,
-            s_ArraySize == 1 ? "element" : "elements",
-            p_Id
-        ).c_str()
-    );
+    void* s_Iterator = s_ArrayType->m_pArrayFunctions->begin(p_Data);
+    void* s_End = s_ArrayType->m_pArrayFunctions->end(p_Data);
 
-    ImGui::PopFont();
+    int s_Index = 0;
 
-    if (s_IsTreeNodeOpen) {
-        for (size_t i = 0; i < s_ArraySize; ++i) {
-            uint8_t* s_ElementPtr = s_BasePtr + i * s_ElementSize;
+    for (; s_Iterator != s_End; s_Iterator = s_ArrayType->m_pArrayFunctions->next(p_Data, s_Iterator), ++s_Index) {
+        ImGui::PushID(static_cast<int>(s_Index));
 
-            ImGui::PushID(static_cast<int>(i));
+        ImGui::Separator();
 
-            ImGui::Separator();
+        ImGui::AlignTextToFramePadding();
+        ImGui::Text("[%zu]", s_Index);
 
-            ImGui::AlignTextToFramePadding();
-            ImGui::Text("[%zu]", i);
+        ImGui::SameLine();
 
-            ImGui::SameLine();
+        const std::string s_ElementId = std::format("{}[{}]", p_Id, s_Index);
 
-            const std::string s_ElementId = std::format("{}[{}]", p_Id, i);
+        DrawEntityPropertyValue(
+            s_ElementId,
+            "",
+            s_ElementTypeName,
+            s_ElementTypeID,
+            p_Entity,
+            p_Property,
+            s_Iterator
+        );
 
-            DrawEntityPropertyValue(
-                s_ElementId,
-                "",
-                s_ElementTypeName,
-                s_ElementTypeID,
-                p_Entity,
-                p_Property,
-                s_ElementPtr
-            );
-
-            ImGui::PopID();
-        }
-
-        ImGui::TreePop();
+        ImGui::PopID();
     }
 }
