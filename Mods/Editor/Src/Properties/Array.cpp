c57ef448982e43fd2357bb3b3427497a9f89862e
Pavle <pavle_nis@yahoo.com>
2026-01-16 07:49:52 +0100

    Use functions from SArrayFunctions instead of manual array pointer arithmetic

--- Mods/Editor/Src/Properties/Array.cpp
+++ Mods/Editor/Src/Properties/Array.cpp
@@ -16,28 +16,58 @@ void Editor::TArrayProperty(const std::string& p_Id, ZEntityRef p_Entity, ZEntit
     }
 
     const IArrayType* s_ArrayType = static_cast<IArrayType*>(p_TypeID->typeInfo());
-    const STypeID* s_ElementTypeID = s_ArrayType->m_pArrayElementType;
-    const std::string s_ElementTypeName = s_ElementTypeID->typeInfo()->m_pTypeName;
-    const size_t s_ElementSize = s_ElementTypeID->typeInfo()->m_nTypeSize;
-    const size_t s_ElementAlign = s_ElementTypeID->typeInfo()->m_nTypeAlignment;
+    size_t s_ArraySize = s_ArrayType->m_pArrayFunctions->size(p_Data);
 
-    size_t s_ArraySize;
+    ImGui::PushFont(SDK()->GetImGuiBoldFont());
+    ImGui::AlignTextToFramePadding();
 
-    if (s_Array->fitsInline() && s_Array->hasInlineFlag()) {
-        s_ArraySize = s_Array->m_nInlineCount;
+    const bool s_IsTreeNodeOpen = ImGui::TreeNodeEx(
+        fmt::format("{} ({} {})##{}",
+            s_PropertyName,
+            s_ArraySize,
+            s_ArraySize == 1 ? "element" : "elements",
+            p_Id
+        ).c_str()
+    );
+
+    ImGui::PopFont();
+
+    if (s_IsTreeNodeOpen) {
+        const STypeID* s_ElementTypeID = s_ArrayType->m_pArrayElementType;
+        const std::string s_ElementTypeName = s_ElementTypeID->typeInfo()->m_pTypeName;
+
+        void* s_Iterator = s_ArrayType->m_pArrayFunctions->begin(p_Data);
+        void* s_End = s_ArrayType->m_pArrayFunctions->end(p_Data);
+
+        int index = 0;
+        for (; s_Iterator != s_End; s_Iterator = s_ArrayType->m_pArrayFunctions->next(p_Data, s_Iterator), ++index) {
+            ImGui::PushID(static_cast<int>(index));
+
+            ImGui::Separator();
+
+            ImGui::AlignTextToFramePadding();
+            ImGui::Text("[%zu]", index);
+
+            ImGui::SameLine();
+
+            const std::string s_ElementId = std::format("{}[{}]", p_Id, index);
+
+            DrawEntityPropertyValue(
+                s_ElementId,
+                "",
+                s_ElementTypeName,
+                s_ElementTypeID,
+                p_Entity,
+                p_Property,
+                s_Iterator
+            );
+
+            ImGui::PopID();
+        }
+
+        ImGui::TreePop();
     }
-    else {
-        s_ArraySize = (reinterpret_cast<uintptr_t>(s_Array->m_pEnd) -
-            reinterpret_cast<uintptr_t>(s_Array->m_pBegin)) / s_ElementSize;
-    }
-
-    uint8_t* s_BasePtr;
-
-    if (s_Array->fitsInline() && s_Array->hasInlineFlag()) {
-        s_BasePtr = reinterpret_cast<uint8_t*>(&s_Array->m_pBegin);
-    }
-    else {
-        s_BasePtr = reinterpret_cast<uint8_t*>(s_Array->m_pBegin);
+}
     }
 
     ImGui::PushFont(SDK()->GetImGuiBoldFont());
