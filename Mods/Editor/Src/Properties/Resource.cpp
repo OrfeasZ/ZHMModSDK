613f9ce4bf784458a0539958b26c76b00df22e25
Pavle <pavle_nis@yahoo.com>
2026-01-17 11:03:02 +0100

    Add colored text for invalid resource pointer and runtime resource ID

--- Mods/Editor/Src/Properties/Resource.cpp
+++ Mods/Editor/Src/Properties/Resource.cpp
@@ -10,9 +10,14 @@ void Editor::ResourcePtrProperty(const std::string& p_Id, ZEntityRef p_Entity, Z
     if (s_ResourcePtr && s_ResourcePtr->m_nResourceIndex.val >= 0) {
         const auto& rid = s_ResourcePtr->GetResourceInfo().rid;
         s_RuntimeResourceID = fmt::format("{:08X}{:08X}", rid.m_IDHigh, rid.m_IDLow);
-    }
 
-    ImGui::TextUnformatted(s_RuntimeResourceID.c_str());
+        ImGui::TextUnformatted(s_RuntimeResourceID.c_str());
+    }
+    else {
+        constexpr ImVec4 s_TextColor = ImVec4(1.f, 1.f, 1.f, 0.5f);
+
+        ImGui::TextColored(s_TextColor, "(%s)", "null");
+    }
 
     if (ImGuiCopyWidget(("ResourcePtr_" + p_Id).c_str())) {
         CopyToClipboard(s_RuntimeResourceID.c_str());
@@ -25,9 +30,14 @@ void Editor::ZRuntimeResourceIDProperty(const std::string& p_Id, ZEntityRef p_En
 
     if (*s_RuntimeResourceID != -1) {
         s_RuntimeResourceIDString = fmt::format("{:08X}{:08X}", s_RuntimeResourceID->m_IDHigh, s_RuntimeResourceID->m_IDLow);
-    }
 
-    ImGui::TextUnformatted(s_RuntimeResourceIDString.c_str());
+        ImGui::TextUnformatted(s_RuntimeResourceIDString.c_str());
+    }
+    else {
+        constexpr ImVec4 s_TextColor = ImVec4(1.f, 1.f, 1.f, 0.5f);
+
+        ImGui::TextColored(s_TextColor, "(%s)", "null");
+    }
 
     if (ImGuiCopyWidget(("ResourcePtr_" + p_Id).c_str())) {
         CopyToClipboard(s_RuntimeResourceIDString.c_str());
