a1a8b57f50382582545b37a7ecfefaa6caf7642f
Pavle <pavle_nis@yahoo.com>
2026-01-31 09:15:40 +0100

    Add m_actorGridNode to ZKnowledge and add padding to match size of ZKnowledge

--- ZHMModSDK/Include/Glacier/ZKnowledge.h
+++ ZHMModSDK/Include/Glacier/ZKnowledge.h
@@ -1,3 +1,13 @@
 #pragma once
 
-class ZKnowledge {};
+#include "Common.h"
+#include "ZGrid.h"
+
+class ZKnowledge {
+public:
+    PAD(0x20);
+    ZGridNodeRef m_actorGridNode; // 0x20
+    PAD(0xD0);
+};
+
+static_assert(sizeof(ZKnowledge) == 256);
