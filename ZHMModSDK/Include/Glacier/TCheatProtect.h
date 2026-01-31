e65db5c30657cc31b9dbb44c0a8967c7e7464473
Pavle <pavle_nis@yahoo.com>
2026-01-31 09:22:30 +0100

    Add TCheatProtect

---
+++ ZHMModSDK/Include/Glacier/TCheatProtect.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "ZPrimitives.h"
+
+template <typename T>
+class TCheatProtect {
+public:
+    struct SStorage {
+        union {
+            T m_tValue;
+            uint8 m_aValueMemory[sizeof(T)];
+        };
+
+        union {
+            uint32* m_piChecksum;
+            uint8 m_aChecksumMemory[sizeof(uint32)];
+        };
+    };
+
+    SStorage m_Storage;
+};
