d7d3df8206ec954237aed945f4706fc0c03588ff
Pavle <pavle_nis@yahoo.com>
2026-01-31 09:14:15 +0100

    Move ZBehaviorService and SBehaviorBase from ZKnowledge.h to ZBehavior.h

--- ZHMModSDK/Include/Glacier/ZKnowledge.h
+++ ZHMModSDK/Include/Glacier/ZKnowledge.h
@@ -1,25 +1,3 @@
 #pragma once
 
-#include "ZPrimitives.h"
-#include "TArray.h"
-
 class ZKnowledge {};
-
-struct SBehaviorBase {
-    ECompiledBehaviorType eBehaviorType;
-};
-
-class ZBehaviorService {
-public:
-    virtual ~ZBehaviorService() = 0;
-
-    struct SBehaviorState {
-        PAD(0xA0); // 0x0
-        SBehaviorBase* m_pCurrentBehavior; // 0xA0
-        PAD(0x50); // 0xA8
-    };
-
-    TFixedArray<SBehaviorState, 500> m_aBehaviorStates;
-};
-
-static_assert(sizeof(ZBehaviorService::SBehaviorState) == 248);
