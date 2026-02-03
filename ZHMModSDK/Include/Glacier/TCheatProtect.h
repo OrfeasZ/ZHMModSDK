957fccfd227ee04fa28de7f76f2d99724dc8278c
Pavle <pavle_nis@yahoo.com>
2026-02-03 18:17:00 +0100

    Implement sizeWithFlags method in ZString and fix loop in SetValue method in TCheatProtect

--- ZHMModSDK/Include/Glacier/TCheatProtect.h
+++ ZHMModSDK/Include/Glacier/TCheatProtect.h
@@ -27,38 +27,51 @@ template <>
 class TCheatProtect<ZString> {
 public:
     ZString GetValue() {
-        const uint32_t s_Length = m_sScrambledString.size();
+        const uint32_t s_Size = m_sScrambledString.size();
 
-        std::vector<uint8_t> s_Buffer(s_Length);
-
-        memcpy(s_Buffer.data(), m_sScrambledString.c_str(), s_Length);
-
-        for (uint32_t i = 0; i < s_Length - 1; ++i) {
-            s_Buffer[i] ^= static_cast<uint8_t>(s_Buffer[i + 1] + m_sScrambledString.m_nLength);
+        if (s_Size == 0) {
+            return ZString();
         }
 
-        s_Buffer[s_Length - 1] ^= static_cast<uint8_t>(m_sScrambledString.m_nLength);
+        const uint32_t s_SizeWithFlags = m_sScrambledString.sizeWithFlags();
+
+        std::vector<uint8_t> s_Buffer(s_Size);
+
+        memcpy(s_Buffer.data(), m_sScrambledString.c_str(), s_Size);
+
+        for (uint32_t i = 0; i < s_Size - 1; ++i) {
+            s_Buffer[i] ^= static_cast<uint8_t>(s_Buffer[i + 1] + s_SizeWithFlags);
+        }
+
+        s_Buffer[s_Size - 1] ^= static_cast<uint8_t>(s_SizeWithFlags);
 
         return ZString::AllocateFromCStr(
-            reinterpret_cast<const char*>(s_Buffer.data()), s_Length
+            reinterpret_cast<const char*>(s_Buffer.data()), s_Size
         );
     }
 
     void SetValue(const ZString& s_Value) {
-        const uint32_t s_Length = m_sScrambledString.size();
+        const uint32_t s_Size = m_sScrambledString.size();
 
-        std::vector<uint8_t> s_Buffer(s_Length);
+        if (s_Size == 0) {
+            m_sScrambledString = ZString();
+            return;
+        }
 
-        memcpy(s_Buffer.data(), s_Value.c_str(), s_Length);
+        const uint32_t s_SizeWithFlags = m_sScrambledString.sizeWithFlags();
 
-        s_Buffer[s_Length - 1] ^= static_cast<uint8_t>(s_Value.m_nLength);
+        std::vector<uint8_t> s_Buffer(s_Size);
 
-        for (uint32_t i = s_Length - 2; i >= 0; --i) {
-            s_Buffer[i] ^= static_cast<uint8_t>(s_Buffer[i + 1] + s_Value.m_nLength);
+        memcpy(s_Buffer.data(), s_Value.c_str(), s_Size);
+
+        s_Buffer[s_Size - 1] ^= static_cast<uint8_t>(s_SizeWithFlags);
+
+        for (int32_t i = static_cast<int32_t>(s_Size) - 2; i >= 0; --i) {
+            s_Buffer[i] ^= static_cast<uint8_t>(s_Buffer[i + 1] + s_SizeWithFlags);
         }
 
         m_sScrambledString = ZString::AllocateFromCStr(
-            reinterpret_cast<const char*>(s_Buffer.data()), s_Length
+            reinterpret_cast<const char*>(s_Buffer.data()), s_Size
         );
     }
 
